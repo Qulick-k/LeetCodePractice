6337ed21425987bf43ff63c2868c6f72df0d4db0
qulick <58820014+Qulick-k@users.noreply.github.com>
2024-08-29 01:34:03 +0800

    [2542] Maximum Subsequence Score 練習Heap / PriorityQueue，解題第一步的重心在nums2的排序和訪問，蓋牌重打
    [2542] Maximum Subsequence Score 練習Heap / PriorityQueue，解題第一步的重心在nums2的排序和訪問，蓋牌重打

--- 2542.maximum-subsequence-score.cs
+++ 2542.maximum-subsequence-score.cs
@@ -8,50 +8,105 @@
 public class Solution {
     public long MaxScore(int[] nums1, int[] nums2, int k)
     {
-        // 
-            var queue = new PriorityQueue<int, int>(new IntDescending());
-            var queue2 = new PriorityQueue<int, int>();
+        //設置兩組PriorityQueue，queue_Pair這組要把優先順序改成由大排到小
+        PriorityQueue<int, int> queue_Pair = new PriorityQueue<int, int>(new IntDescending());
+        PriorityQueue<int, int> queue_Reduce = new PriorityQueue<int, int>();
 
 
-            for (int i = 0; i < nums1.Length; i++)
-                queue.Enqueue(nums1[i], nums2[i]);
+        //把nums1作為一般數值、nums2作為優先順序的數值，放進queue_Pair
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            queue_Pair.Enqueue(nums1[i], nums2[i]);
+        }
 
-            long sum = 0L;
-            int min = int.MaxValue;
+        //設置nums1的起始總和，以及nums2的最小比較值
+        long sum = 0;
+        int min = int.MaxValue;
 
-            for (int i = 0; i < k; i++)
-            {
-                queue.TryDequeue(out int num1, out int num2);
-                sum += num1;
-                min = num2;
-                queue2.Enqueue(num1, num1);
-            }
+        //以K作為迴圈長度上限，把queue_Pair的一般數值和順序數值都提取成num1、num2
+        //最後獲得加了k次的sum，以及第k個最小的min數值
+        for (int i = 0; i < k; i++)
+        {
+            queue_Pair.TryDequeue(out int num1, out int num2);
+            sum += num1;
+            min = num2;
+            //把每一次從queue_Pair提取出來的num1，放進queue_Reduce作為一般數值和順序數值
+            queue_Reduce.Enqueue(num1, num1);
+        }
+        Console.WriteLine(sum);
+        Console.WriteLine(min);
+        //起始總和乘上nums2的2第k個最小值，作為暫時起始分數score
+        //並且把score賦予給maxScore，作為之後分數的大小比較標準
+        long score = sum * min;
+        long maxScore = score;
 
-            long score = sum * min;
-            long maxScore = score;
-
-            for (int i = k; i < nums1.Length; i++)
-            {
-                sum -= queue2.Dequeue();
-                queue.TryDequeue(out int num1, out int num2);
-                sum += num1;
-                min = num2;
-                score = sum * min;
-                maxScore = Math.Max(maxScore, score);
-                queue2.Enqueue(num1, num1);
-            }
-
-            return maxScore;
+        //從k值開始進行迴圈，直到nums1的長度上限。以Test case 1 為例
+        //nums1 = [1,3,3,2], nums2 = [2,1,3,4], k = 3 ， i < nums1.Length == 4
+        for (int i = k; i < nums1.Length; i++)
+        {
+            sum -= queue_Reduce.Dequeue();                     // sum = 6 - 1 = 5
+            queue_Pair.TryDequeue(out int num1, out int num2); // nums1 = 3 ,nums2 = 1
+            sum += num1;                                       // sum = 5 + 3 == 8
+            min = num2;                                        // min = 1 
+            score = sum * min;                                 // score = 8 * 1 == 8
+            maxScore = Math.Max(maxScore, score);              // Math.Max(12, 8) == 12
+            queue_Reduce.Enqueue(num1, num1);                  // Enqueue(3, 3)
+        }
+        //回傳最大值
+        return maxScore; 
     }
 }
-    public class IntDescending : IComparer<int>
-    {        
-        public int Compare(int x, int y) => y - x;
-    }
+public class IntDescending : IComparer<int>
+{        
+    public int Compare(int x, int y) => y - x; //用在PriorityQueue，就代表把優先排序數值，改成降冪排序
+}
+
 // @lc code=end
+
+/*蓋牌重打
+        PriorityQueue<int, int> Queue_Pair = new PriorityQueue<int, int>( new IntDescending());
+        PriorityQueue<int, int> Queue_Reduce = new PriorityQueue<int, int>();
+        
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            Queue_Pair.Enqueue(nums1[i], nums2[i]);
+        }
+
+        long sum = 0;
+        int min = int.MaxValue;
+        for (int i = 0; i < k; i++)
+        {
+            Queue_Pair.TryDequeue( out int num1, out int num2);
+            sum = sum + num1;
+            min = num2;
+            Queue_Reduce.Enqueue(num1, num1);
+        }
+        
+        long Score = sum * min;
+        long Max = Score;
+
+        for(int i = k; i < nums1.Length; i++)
+        {
+            sum = sum - Queue_Reduce.Dequeue();
+            Queue_Pair.TryDequeue(out int num1, out int num2);
+            sum = sum + num1;
+            min = num2;
+            Score = sum * min;
+            Max = Math.Max(Max, Score);
+            Queue_Reduce.Enqueue(num1, num1);
+        }
+
+        return Max;
+
+public class IntDescending : IComparer<int>
+{
+    public int Compare(int x, int y) => y - x;
+}        
+*/
 
 /* PriorityQueue<TElement,TPriority>.Comparer 屬性
 https://learn.microsoft.com/zh-tw/dotnet/api/system.collections.generic.priorityqueue-2.comparer?view=net-8.0#system-collections-generic-priorityqueue-2-comparer
+https://learn.microsoft.com/zh-tw/dotnet/api/system.collections.generic.comparer-1?view=net-8.0
 */
 
 /* 利用Comparer把PriorityQueue的排序，改成由大到小
@@ -62,6 +117,22 @@ https://stackoverflow.com/questions/71295930/c-sharp-version-of-priorityqueue-fo
 /* Lambda介紹
 https://medium.com/@newpage0720/%E5%B0%8Dc-%E7%9A%84-lambda-%E7%9A%84%E7%90%86%E8%A7%A3-438e6de01305
 */
+
+        ///以test case 1 為例，有無排大小的差別
+        ///PriorityQueue<int, int> queue_test1 = new PriorityQueue<int, int>(new IntDescending());
+        ///PriorityQueue<int, int> queue_test2 = new PriorityQueue<int, int>();
+        ///for (int i = 0; i < nums1.Length; i++)
+        ///{
+        ///    queue_test1.Enqueue(nums1[i], nums2[i]);
+        ///    queue_test2.Enqueue(nums1[i], nums2[i]);
+        ///}
+        ///for (int i = 0; i < nums1.Length; i++)
+        ///{
+        ///    queue_test1.TryDequeue( out int nums01, out int nums02);
+        ///    queue_test2.TryDequeue( out int nums03, out int nums04);
+        ///    Console.WriteLine("這個是nums2:" + nums02); // 4 3 2 1
+        ///    Console.WriteLine("這個是nums4:" + nums04); // 1 2 3 4
+        ///}*/
 
 /*      //自己寫的爛扣，失敗 10 /28 cases passed (N/A)
         int Score = 0;
