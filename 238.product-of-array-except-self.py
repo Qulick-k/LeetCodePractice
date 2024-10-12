e9fcfe41c7ce752652993eb8e8312212789429ba
qulick <58820014+Qulick-k@users.noreply.github.com>
2024-10-12 17:04:55 +0800

    [238] Product of Array Except Self  #複習two pointer ，使用python
    [238] Product of Array Except Self  #複習two pointer ，使用python

---
+++ 238.product-of-array-except-self.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=238 lang=python3
+#
+# [238] Product of Array Except Self
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        #複習two pointer
+        left_sum = 1
+        right_sum = 1
+        res = [0] * len(nums)
+        for i in range(len(nums)):
+            res[i] = left_sum 
+            left_sum *= nums[i]
+        
+        for i in range(len(nums)-1, -1, -1):
+            res [i] *= right_sum
+            right_sum *= nums[i]
+        return res
+# @lc code=end
+
