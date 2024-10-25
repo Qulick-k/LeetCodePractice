46b4f969a7240aa2d632a121066170128213f994
qulick <58820014+Qulick-k@users.noreply.github.com>
2024-10-26 03:00:27 +0800

    [70] Climbing Stairs 練習基礎DP還有Memorization
    [70] Climbing Stairs 練習基礎DP還有Memorization

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -12,13 +12,15 @@ class Solution:
         three = 3
         remainder = 0
 
-        if n == 2:
+        if n <= 3:
+            return n
+        '''if n == 2:
             return 2
         elif n == 3:
             return 3
         elif n == 1:
             return 1
-
+        '''
         for i in range(4,n+1):
             remainder = three + two
             two = three
