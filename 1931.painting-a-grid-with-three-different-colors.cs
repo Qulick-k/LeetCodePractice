b7506b10cd67ef90f2ab39fbb85143fbcffb2105
qulick <58820014+Qulick-k@users.noreply.github.com>
2025-05-19 02:26:02 +0800

    處理C++的COMCILER
    處理C++的COMCILER

---
+++ 1931.painting-a-grid-with-three-different-colors.cs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1931 lang=csharp
+ *
+ * [1931] Painting a Grid With Three Different Colors
+ */
+
+// @lc code=start
+public class Solution {
+    public int ColorTheGrid(int m, int n) {
+        
+    }
+}
+// @lc code=end
+
